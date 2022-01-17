67427746c217e30ab0e9f2ffe4edb7b1015e8d73
arvsites <arvsites@gmail.com>
2022-01-17 23:34:15 +0500

    fide port

--- main.py
+++ main.py
@@ -2,10 +2,59 @@ import mido
 import serial
 import time
 
+list=['COM1','COM2','COM3','COM4','COM5','COM6','COM7','COM8','COM9','COM10','COM11','COM12','COM13','COM14','COM15','COM16','COM17','COM18',]
 
-port='COM10' #порт midi устройства
-arduino=serial.Serial(port, 115200)
-time.sleep(2)
+
+
+COM1='COM1'
+COM2='COM2'
+COM3='COM3'
+COM4='COM4'
+COM5='COM5'
+COM6='COM6'
+COM7='COM7'
+COM8='COM8'
+COM9='COM9'
+COM10='COM10'
+COM11='COM11'
+COM12='COM12'
+COM13='COM13'
+COM14='COM14'
+COM15='COM15'
+COM16='COM16'
+COM17='COM17'
+COM18='COM18'
+COM19='COM19'
+time.sleep(1)
+arduino = serial.Serial()
+
+arduino.baudrate = 115200
+
+i=1
+
+while True:
+    time.sleep(.2)
+    print(i)
+    arduino.port = list[i]
+    try:
+
+        arduino.open()
+        if arduino.isOpen()==True:
+            print('connected')
+            #print('arduino is on COMPORT'.join(i))
+            break
+        break
+
+    except:
+        print('waiting')
+        i=i+1
+        if i==18:
+            print('Kindly remove usb cable and try again')
+            break
+
+print('here we go')
+print(i+1)
+
 
 note = [35,37,38,41,44,45,47,48,49] #ноты midi устройства
 
